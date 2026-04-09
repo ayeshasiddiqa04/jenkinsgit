b7b3c0a8baea5d6739d4840ad757d768743dcb44
ayeshasiddiqa04“git config --global user.name ayeshasiddiqa04“ <ayeshsiddiqa04@gmail.com>
2026-04-09 15:38:34 +0530

    first commit

---
+++ src/main/java/com/example/jenkinsgit/App.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
